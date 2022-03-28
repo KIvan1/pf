1e98706efe88a5827d8d74049ebed0874fb8f7bf
KIvan1 <kirillov20403@gmail.com>
2022-03-28 20:54:15 +0300

    commit

--- python_app4/main.py
+++ python_app4/main.py
@@ -1,6 +1,20 @@
 import Uplode
 import Open
+import tkinter as tk
 
 jsonData = Uplode.fuplode()
+window = tk.Tk()
+text = []
+for i in range(5):
+	text.append(tk.Label(text = str(i), width = 50))
+	text[i].pack()
 
-print(jsonData["name"])
+entry = tk.Entry(width = 50, fg = "blue")
+entry.insert(0, "hello")
+entry.pack()
+button = tk.Button(text = "OK", width = 10, height = 2, fg = "blue")
+button.pack()
+s = entry.get()
+print(s)
+window.mainloop()
+print(s)
