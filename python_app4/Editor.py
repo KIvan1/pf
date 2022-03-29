0746c8e2b9dca1ca2dac762e5723cea9e0eda875
KIvan1 <kirillov20403@gmail.com>
2022-03-29 12:14:37 +0300

    commit

--- python_app4/Editor.py
+++ python_app4/Editor.py
@@ -7,7 +7,9 @@ def json_editor(jsonData):
 	j = 0
 	for i in jsonData:
 		name.append(tk.Label(text = i))
-		name[j].pack()
+		name[j].pack(side = tk.LEFT)
+		data.append(tk.Entry(text = jsonData[i]))
+		data[j].pack(side = tk.LEFT)
 		j += 1
 	ed_window.mainloop()
 	return data
