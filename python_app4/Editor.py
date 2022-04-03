f6cb0e4ec574e06c409d78fe0f5f85f7582f77d9
KIvan1 <kirillov20403@gmail.com>
2022-04-03 21:16:46 +0300

    commit

--- python_app4/Editor.py
+++ python_app4/Editor.py
@@ -1,12 +1,9 @@
 import tkinter as tk
+from tkinter.filedialog import asksaveasfilename
+import json
 
 def json_editor(jsonData):
-	def change_data():
-		k = 0
-		for i in jsonData:
-			jsonData[i] = newData[k].get()
-			k += 1
-
+	file_way = None
 	def add():
 		add_str = {key.get(): addData.get()}
 		jsonData.update(add_str)
@@ -22,12 +19,39 @@ def json_editor(jsonData):
 
 		ed_window.update()
 
+	def save_as():
+		filepath = asksaveasfilename()
+		global file_way
+		file_way = filepath
+		if not filepath:
+			return
+		with open(filepath, "w") as json_out_file:
+			k = 0
+			for i in jsonData:
+				jsonData[i] = newData[k].get()
+				k += 1
+			json.dump(jsonData, json_out_file)
+
+	def save():
+		global file_way
+		if file_way == None:
+			save_as()
+		else:
+			with open(file_way, "w") as json_out_file:
+				k = 0
+				for i in jsonData:
+					jsonData[i] = newData[k].get()
+					k += 1
+				json.dump(jsonData, json_out_file)
+
 	ed_window = tk.Tk()
 	ed_window.title("Json editor")
 
 	save_frame = tk.Frame(ed_window)
-	save_button = tk.Button(save_frame, text = "Save", command = change_data)
+	save_as_button = tk.Button(save_frame, text = "Save as", command = save_as)
+	save_button = tk.Button(save_frame, text = "Save", command = save)
 	save_button.pack(side = tk.LEFT)
+	save_as_button.pack(side = tk.LEFT)
 	save_frame.pack()
 	
 	add_frame = tk.Frame(ed_window)
@@ -50,8 +74,13 @@ def json_editor(jsonData):
 	newData = []
 	json_frame = []
 	j = 0
+	parrent_frame = tk.Frame(ed_window)
+	canvas = tk.Canvas(parrent_frame)
+	ed_frame = tk.Frame(parrent_frame)
+	ed_frame.pack(fill = tk.X)
+	scroll = tk.Scrollbar(parrent_frame, command = canvas.yview)
 	for i in jsonData:
-		json_frame.append(tk.Frame(ed_window))
+		json_frame.append(tk.Frame(ed_frame))
 		name.append(tk.Label(json_frame[j], text = i + ': '))
 		name[j].pack(fill = tk.X, side = tk.LEFT)
 		newData.append(tk.StringVar())
@@ -61,5 +90,14 @@ def json_editor(jsonData):
 		json_frame[j].pack(fill = tk.X)
 		j += 1
 
+	canvas.create_window(0, 0, anchor = 'nw', window = ed_frame)
+	canvas.update_idletasks()
+	canvas.configure(scrollregion = canvas.bbox('all'), 
+                 yscrollcommand = scroll.set)               
+	canvas.pack(fill = tk.BOTH, expand = True, side = tk.LEFT)
+	scroll.pack(side = tk.RIGHT, fill = tk.Y)
+	parrent_frame.pack(fill = tk.BOTH)
+
 	ed_window.mainloop()
+
 	return jsonData
